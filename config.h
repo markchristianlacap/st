335839a513abb31b0fc9ed15a2f0e3a2ecb64114
mark <markchristianlacap@gmail.com>
2024-02-23 23:21:38 +0800

    new config

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "VictorMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetbrainsMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 8;
 
 /*
